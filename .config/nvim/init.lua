d7832f15f4d5884e6cf23948a880524cfcbaecf2
Michael Lan <michaellan202@gmail.com>
2021-05-16 07:48:07 -0700

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,30 +7,20 @@ end
 vim.cmd 'packadd packer.nvim'
 require('packer').startup(function()
   use 'https://github.com/wbthomason/packer.nvim'
-  use 'https://github.com/neovim/nvim-lspconfig'
-  use 'https://github.com/nvim-lua/completion-nvim'
-  use 'https://github.com/nvim-lua/popup.nvim'
-  use 'https://github.com/nvim-lua/plenary.nvim'
   use 'https://github.com/cormacrelf/vim-colors-github'
+  use 'https://github.com/neovim/nvim-lspconfig'
+  use 'https://github.com/arzg/vim-plan9'
   use 'https://github.com/nvim-treesitter/nvim-treesitter'
-  use 'https://github.com/liquidz/vim-iced'
-  use 'https://github.com/guns/vim-sexp'
-  use 'https://github.com/tpope/vim-sexp-mappings-for-regular-people'
   use 'https://github.com/tpope/vim-commentary'
   use 'https://github.com/tpope/vim-repeat'
   use 'https://github.com/machakann/vim-sandwich'
   use 'https://github.com/junegunn/vim-easy-align'
   use 'https://github.com/bfredl/nvim-luadev'
-  use 'https://github.com/danilamihailov/beacon.nvim'
-  use 'https://github.com/ekaj2/limelight.vim'
   use 'https://github.com/janet-lang/janet.vim'
   use 'https://github.com/neomutt/neomutt.vim'
-  use 'https://github.com/mizlan/yui'
-  use 'https://github.com/lifepillar/vim-gruvbox8/'
-  use 'https://github.com/pechorin/any-jump.vim'
   use '~/util/telescope.nvim/'
-  -- use 'https://github.com/hrsh7th/nvim-compe'
-  -- use 'https://github.com/tami5/vim-iced-compe'
+  use 'https://github.com/tpope/vim-unimpaired'
+  use 'https://github.com/romainl/vim-qf'
 end)
 
 --- Options
@@ -64,19 +54,14 @@ vim.cmd [[iabbrev frg Federal Republic of Germany]]
 vim.cmd [[iabbrev gdr German Democratic Republic]]
 vim.cmd [[nn Y "+y]]
 vim.cmd [[vn Y "+y]]
-vim.cmd [[nn ! :Beacon<CR>]]
 vim.cmd [[nmap ga <Plug>(EasyAlign)]]
--- vim.g.sexp_enable_insert_mode_mappings = 0
-
-
---- Telescope
-vim.api.nvim_set_keymap('n', '<Leader>.', ':Telescope find_files previewer=false<CR>', {silent = true, noremap = true})
-vim.cmd[[highlight link TelescopeSelection CursorLine]]
+vim.cmd [[syntax sync fromstart]]
 
 --- Language Servers
 local on_attach = function(_, bufnr)
-  require('completion').on_attach()
+  -- require('completion').on_attach()
   local opts = { noremap=true, silent=true }
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gla', '<cmd>lua vim.lsp.buf.code_action()                               <CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'glD', '<cmd>lua vim.lsp.buf.declaration()                               <CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gld', '<cmd>lua vim.lsp.buf.definition()                                <CR>', opts)
@@ -92,12 +77,10 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '[e',  '<cmd>lua vim.lsp.diagnostic.goto_prev()                          <CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', ']e',  '<cmd>lua vim.lsp.diagnostic.goto_next()                          <CR>', opts)
 end
-require'lspconfig'.hls.setup{
-  on_attach = on_attach
-}
-require'lspconfig'.clangd.setup{
-  on_attach = on_attach
-}
+
+require'lspconfig'.hls.setup{ on_attach = on_attach }
+require'lspconfig'.tsserver.setup{ on_attach = on_attach }
+require'lspconfig'.clangd.setup{ on_attach = on_attach }
 
 --- Treesitter
 require('nvim-treesitter.configs').setup {
@@ -115,44 +98,6 @@ require('nvim-treesitter.configs').setup {
 }
 vim.cmd 'command! TSRehighlight :write | edit | TSBufEnable highlight'
 
---- Compe
--- require'compe'.setup {
---   enabled = true;
---   autocomplete = true;
---   debug = false;
---   min_length = 1;
---   preselect = 'enable';
---   throttle_time = 80;
---   source_timeout = 200;
---   incomplete_delay = 400;
---   max_abbr_width = 100;
---   max_kind_width = 100;
---   max_menu_width = 100;
---   documentation = true;
-
---   source = {
---     path = true;
---     buffer = true;
---     calc = true;
---     nvim_lsp = true;
---     nvim_lua = true;
---     vsnip = true;
---   };
--- }
-
---- Clojure
-
--- ARCHIVED: using regular-people plugin
--- vim.g.sexp_mappings = {
---   sexp_emit_head_element = 'gH',
---   sexp_emit_tail_element = 'gL',
---   sexp_capture_prev_element = 'gh',
---   sexp_capture_next_element = 'gl',
--- }
-vim.g.iced_default_key_mapping_leader = "<Localleader>"
-vim.g.iced_enable_default_key_mappings = true
-vim.g.iced_enable_clj_kondo_analysis = true
-
 --- Experiment
 vim.cmd [[
   nmap <C-c><C-c> <Plug>(Luadev-RunLine)
@@ -161,3 +106,21 @@ vim.cmd [[
 ]]
 vim.cmd[[nn gK viw"by:h <C-r>b<CR>]]
 vim.cmd[[nn <Localleader>ld :Luadev<CR>:wincmd k<CR>:wincmd L<CR>:wincmd p<CR>]]
+
+vim.cmd [[
+  command! -range=% SP <line1>,<line2>w !curl -F 'sprunge=<-' http://sprunge.us | tr -d '\n' | pbcopy
+  command! -range=% CL <line1>,<line2>w !curl -F 'clbin=<-' https://clbin.com | tr -d '\n' | pbcopy
+  command! -range=% VP <line1>,<line2>w !curl -F 'text=<-' http://vpaste.net | tr -d '\n' | pbcopy
+  command! -range=% PB <line1>,<line2>w !curl -F 'c=@-' https://ptpb.pw/?u=1 | tr -d '\n' | pbcopy
+  command! -range=% IX <line1>,<line2>w !curl -F 'f:1=<-' http://ix.io | tr -d '\n' | pbcopy
+  command! -range=% EN <line1>,<line2>w !curl -F 'file=@-;' https://envs.sh | tr -d '\n' | pbcopy
+  command! -range=% TB <line1>,<line2>w !nc termbin.com 9999 | tr -d '\n' | pbcopy
+  ]]
+
+--- imenu system
+vim.cmd [[
+  let &grepprg = 'rg --vimgrep $* -o'
+  autocmd BufEnter *.clj let w:imenu_term = '''defn? ([A-Za-z0-9_-]+)'''
+  autocmd BufEnter *.hs let w:imenu_term = '''(\w+)\s*::'''
+  command! -nargs=0 Imenu execute 'silent lgrep! ' . w:imenu_term . ' ' . expand('%') | lopen
+  ]]
