6f103d5199eeb6849e856e8ff998f4cea51cbcce
Michael Lan <michaellan202@gmail.com>
2022-03-16 09:31:33 -0700

    Add yabai refresher

---
+++ scripts/yabai_void.pl
@@ -0,0 +1,36 @@
+use feature 'say';
+use strict;
+use warnings;
+
+my $skhd_path = "$ENV{HOME}/.config/skhd/skhdrc";
+
+open(SKHD, '<', $skhd_path) or die $!;
+
+my $skhd_text  = join "", <SKHD>;
+
+my %keybind_hash = (
+  Firefox   => q(cmd + shift - return),
+  Messenger => q(cmd + shift - m),
+  iTerm2    => q(cmd + shift - e),
+  Discord   => q(cmd + shift - d),
+);
+
+my @data = qx(yabai -m query --windows | jq '.[] | [.id, .app, .space] | join (" ")');
+
+my @add_lines;
+
+for (@data) {
+  /^"(\d+) (.+) (\d+)"$/;
+
+  if (my $keybind = $keybind_hash{$2}) {
+    push @add_lines, "$keybind : yabai -m space --focus $3; yabai -m window --focus $1";
+    delete($keybind_hash{$2});
+  }
+}
+
+my $add_text = join "\n", @add_lines;
+
+$skhd_text =~ s/^(# speed-dial.*?\n).*/$1$add_text/ms;
+
+open(SKHD, '>', $skhd_path) or die $!;
+say SKHD $skhd_text
