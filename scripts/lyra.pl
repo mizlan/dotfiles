440a75ef7bde8515f2f17e373d3221a150efef24
Michael Lan <michaellan202@gmail.com>
2022-03-27 20:49:36 -0700

    Fix bug with quotation marks
    * more portable

--- scripts/lyra.pl
+++ scripts/lyra.pl
@@ -1,8 +1,8 @@
-use Mac::Pasteboard;
 use Cwd 'abs_path';
+use IPC::Run3;
 
 # save old text, a la vim's savecpo
-my $textSave = pbpaste();
+my $textSave = `pbpaste`;
 
 # run text through perl one-liner
 $s = <STDIN>;
@@ -11,11 +11,11 @@ my ($file, $pg, $text) = $s =~ /(.*):Page (\d+): (.*)/;
 $file = abs_path $file;
 
 # paste edited text
-pbcopy($text);
+run3("pbcopy", \$text);
 qx(psg $file $pg);
 qx(cliclick kd:cmd t:fv ku:cmd kp:enter);
 
 # restore old text
-pbcopy($textSave);
+run3("pbcopy", \$textSave);
 qx(sleep 1);
 qx(cliclick kp:esc);
