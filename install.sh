94cf499eae6e4f6b2c84e4b709f6549ef06e897f
Michael Lan <michaellan202@gmail.com>
2023-05-15 08:37:18 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+stow --no-folding --target="$HOME" zsh
+stow --no-folding --target="$HOME" kitty
+stow --no-folding --target="$HOME" nvim
+stow              --target="$HOME" theming
+stow              --target="$HOME" yabai
+stow              --target="$HOME" skhd
