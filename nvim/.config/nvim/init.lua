6c0dfa295a8d258782575b3416959ce4ebf09e54
Michael Lan <michaellan202@gmail.com>
2023-05-27 23:35:37 -0700

    pog

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.everforest_background = 'soft'
+      vim.g.everforest_background = 'medium'
       vim.g.everforest_better_performance = 1
     end
   },
