0e2bf206f6bbb9c7c066a5df98a035fbb3327c89
Michael Lan <michaellan202@gmail.com>
2024-12-28 09:56:31 -0800

    upd

--- nvim/.config/nvim/queries/ocaml/injections.scm
+++ nvim/.config/nvim/queries/ocaml/injections.scm
@@ -2,4 +2,6 @@
 ((quoted_string
    ((quoted_string_content)
     @injection.content
-    (#set! injection.language "ocaml"))) @_q (#match? @_q "^\\\{sql\\\|"))
+    (#set! injection.language "sql")
+    (#set! injection.include-children)
+    )) @_q (#match? @_q "^\\\{sql\\\|"))
